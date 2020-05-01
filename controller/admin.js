432b4f467be0d143518f93196147c96d38a3d7fa
Jacó Apolinário <jacoapollinario@gmail.com>
2020-05-01 11:12:54 -0300

    settings

--- controller/admin.js
+++ controller/admin.js
@@ -9,6 +9,33 @@ exports.index = function(req, res) {
 exports.create = function(req, res) {
     return res.render("admin/create")
 }
+// Admin Show 
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundRecipes = data.recipes.find(function(recipes) {
+        return recipes.id == id
+    })
+
+    if (!foundRecipes) return res.send("Recipe not found!")
+
+
+    return res.render("admin/show", { recipe: foundRecipes })
+}
+// Edit
+exports.edit = function(req, res) {
+    const { id } = req.params
+
+    const foundRecipes = data.recipes.find(function(recipes) {
+        return recipes.id == id 
+    })
+
+    if (!foundRecipes) return res.send("Recipe not found!")
+
+    return res.render("admin/edit", { recipe: foundRecipes })
+}
+
+
 // Admin Post
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -43,31 +70,6 @@ exports.post = function(req, res) {
     })
 
 } 
-// Admin Show 
-exports.show = function(req, res) {
-    const { id } = req.params
-
-    const foundRecipes = data.recipes.find(function(recipes) {
-        return recipes.id == id
-    })
-
-    if (!foundRecipes) return res.send("Recipe not found!")
-
-
-    return res.render("admin/show", { recipe: foundRecipes })
-}
-// Edit
-exports.edit = function(req, res) {
-    const { id } = req.params
-
-    const foundRecipes = data.recipes.find(function(recipes) {
-        return recipes.id == id 
-    })
-
-    if (!foundRecipes) return res.send("Recipe not found!")
-
-    return res.render("admin/edit", { recipe: foundRecipes })
-}
 // Put
 exports.put = function(req, res) {
     const { id } = req.body
@@ -98,7 +100,7 @@ exports.put = function(req, res) {
         return res.redirect(`/admin/recipes/${id}`)
     })
 }
-
+// Edit
 exports.delete = function(req, res) {
     const { id } = req.body
 
--- routes.js
+++ routes.js
@@ -14,10 +14,11 @@ routes.get('/recipes/:id', recipes.details)
 
 /*=== Routes Admin ===*/
 routes.get('/admin/recipes', admin.index)
-routes.get('/admin/create', admin.create)
-routes.post('/admin/recipes', admin.post)
+routes.get('/admin/recipes/create', admin.create)
 routes.get('/admin/recipes/:id', admin.show)
 routes.get('/admin/recipes/:id/edit', admin.edit)
+
+routes.post('/admin/recipes', admin.post)
 routes.put('/admin/recipes', admin.put)
 routes.delete('/admin/recipes', admin.delete)
 
