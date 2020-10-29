5a379f5c6a4aa137b4844267c85221ffe898b8e0
Jacó Apolinário <jacoapollinario@gmail.com>
2020-10-29 11:22:15 -0300

    refactoring and adding new features

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,3 +7,36 @@ for (let card of cards) {
     })
 }
 
+const PhotosUpload = {
+    uploadLimit: 5,
+    handleFileInput(event) {
+        const { files: filesList } = event.target
+        const { uploadLimit } = PhotosUpload
+
+        if (filesList.length > uploadLimit) {
+            alert(`Envie no maximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return 
+        }
+
+        Array.from(filesList).forEach(file => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = document.createElement('div')
+                div.classList.add('photo')
+
+                div.onclick = () => alert('remover foto')
+
+                div.appendChild(image)
+
+                document.querySelector('#photos-preview').appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    }
+}
