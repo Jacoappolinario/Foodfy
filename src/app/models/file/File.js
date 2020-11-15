b841863450951d94afcf7d07d05db34f7237e7a9
Jacó Apolinário <jacoapollinario@gmail.com>
2020-11-15 07:54:50 -0300

    deleting comment

--- src/app/models/file/File.js
+++ src/app/models/file/File.js
@@ -31,23 +31,5 @@ module.exports = {
         } catch(err) {
             console.error(err)
         }
-
-        // try {
-        //     const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
-        //     const file = result.rows[0]
-
-        //     fs.unlinkSync(file.path)
-
-        //     await db.query(`
-        //         DELETE FROM recipes_files WHERE file_id = $1
-        //     `, [id])
-        //     await db.query(`
-        //         DELETE FROM files WHERE id = $1
-        //     `, [id])
-
-            
-        // } catch(err) {
-        //     console.error(err)
-        //}
     }
 }
