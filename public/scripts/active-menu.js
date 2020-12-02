039683c797fdee32bfb6b2165e32aefb65ab6160
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-02 09:46:58 -0300

    style refactoring

--- public/scripts/active-menu.js
+++ public/scripts/active-menu.js
@@ -1,16 +1,10 @@
 const currentPage = location.pathname
-const menuItensSite =  document.querySelectorAll("header .links a")
-const menuItensAdmin =  document.querySelectorAll("header .links-admin a")
+const menuItens =  document.querySelectorAll("header .primary a")
 
-for (itemSite of menuItensSite) {
+for (itemSite of menuItens) {
     if (currentPage.includes(itemSite.getAttribute("href"))) {
         itemSite.classList.add("active")
     }
 }
 
-for (itemAdmin of menuItensAdmin) {
-    if (currentPage.includes(itemAdmin.getAttribute("href"))) {
-        itemAdmin.classList.add("active")
-    }
-}
 console.log(currentPage)
