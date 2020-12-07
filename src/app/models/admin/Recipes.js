32eea06ed5a41d0951244157cb9736f6b07dbeb8
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-07 10:08:28 -0300

    add try/catch in recipes models

--- src/app/models/admin/Recipes.js
+++ src/app/models/admin/Recipes.js
@@ -3,14 +3,19 @@ const { date } = require('../../../lib/utils')
 
 module.exports = {
     all(){
-        return db.query(`
+        try {
+            return db.query(`
             SELECT recipes.*, chefs.name AS author
             From recipes
             LEFT JOIN chefs ON (chefs.id = recipes.chef_id)
             ORDER BY recipes.id ASC`)
+        } catch(err) {
+            console.error(err)
+        }
     },
     create(data) {
-        const query = `
+        try {
+            const query = `
                     INSERT INTO recipes (
                         chef_id,
                         title,
@@ -20,36 +25,52 @@ module.exports = {
                     ) VALUES ($1, $2, $3, $4, $5)
                     RETURNING id
                     `
-        const values = [
-            data.chef,
-            data.title,
-            data.ingredients,
-            data.preparation,
-            data.information
-        ]
+            const values = [
+                data.chef,
+                data.title,
+                data.ingredients,
+                data.preparation,
+                data.information
+            ]
 
-        return db.query(query, values)
+            return db.query(query, values)
+        } catch(err) {
+            console.error(err)
+        }
     },
     find(id) {
-        return db.query(`
+        try {
+            return db.query(`
                 SELECT recipes.*, chefs.name AS chef_name
                 FROM recipes
                 LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
                 WHERE recipes.id = $1`, [id])
+        } catch(err) {
+            console.error(err)
+        }
     },
     files(id) {
-        return db.query(`
+        try {
+            return db.query(`
             SELECT files.*, recipe_id, file_id 
             FROM files
             LEFT JOIN recipes_files
             ON (files.id = recipes_files.file_id)
             WHERE recipes_files.recipe_id = $1`, [id])
+        } catch(err) {
+            console.error(err)
+        }
     },
     chefSelectOptions() {
-        return db.query(`SELECT id,name FROM chefs`)
+        try {
+            return db.query(`SELECT id,name FROM chefs`)
+        } catch(err) {
+            console.error(err)
+        }
     },
     update(data) {
-        const query = `
+        try {
+            const query = `
             UPDATE recipes SET
                 chef_id=($1),
                 title=($2),
@@ -59,20 +80,28 @@ module.exports = {
                 WHERE id = $6  
             `
         
-        const values = [
-            data.chef,
-            data.title,
-            data.ingredients,
-            data.preparation,
-            data.information,
-            data.id
-        ]
+            const values = [
+                data.chef,
+                data.title,
+                data.ingredients,
+                data.preparation,
+                data.information,
+                data.id
+            ]
 
-        return db.query(query, values) 
+            return db.query(query, values) 
+        } catch(err) {
+            console.error(err)
+        }
     },
     delete(id) {
-        return db.query(`
+        try {
+            return db.query(`
             DELETE FROM recipes 
             WHERE id = $1`, [id])
+
+        } catch(err) {
+            console.error(err)
+        }
     }
 }
