0e727de21cd503b6300baa6d6b668d01d9299ac1
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-07 10:25:29 -0300

    add try/catch in site models

--- src/app/models/site/Site.js
+++ src/app/models/site/Site.js
@@ -2,35 +2,49 @@ const db = require('../../../config/db')
 
 module.exports = {
     all() {
-        return db.query(`
-            SELECT recipes.id, recipes.image, recipes.title,
-            chefs.name AS author
-            FROM recipes
+        try {
+            return db.query(`
+            SELECT recipes.*, chefs.name AS author
+            From recipes
             LEFT JOIN chefs ON (chefs.id = recipes.chef_id)
             ORDER BY recipes.id ASC`)
+        } catch(err) {
+            console.error(err)
+        }
     },
     find(id) {
-        return db.query(`
-        SELECT recipes.*, chefs.name AS chef_name
-        FROM recipes
-        LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-        WHERE recipes.id = $1`, [id])
+        try {
+            return db.query(`
+            SELECT recipes.*, chefs.name AS chef_name
+            FROM recipes
+            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+            WHERE recipes.id = $1`, [id])
+        } catch(err) {
+            console.error(err)
+        }
     },
     findBy(filter) {
-        return db.query(`
-            SELECT recipes.id, recipes.image, recipes.title,
-            chefs.name AS author
+        try {
+            return db.query(`
+            SELECT recipes.*, chefs.name AS chef_name
             FROM recipes
-            LEFT JOIN chefs ON (chefs.id = recipes.chef_id)
+            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
             WHERE recipes.title ILIKE '%${filter}%'
             ORDER BY recipes.id ASC`)
+        } catch(err) {
+            console.error(err)
+        }
     },
     chefs() {
-        return db.query(`
+        try {
+            return db.query(`
             SELECT chefs.*, count(recipes.id) AS total_recipes
             FROM chefs
             LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
             GROUP BY chefs.id
             ORDER BY total_recipes DESC`) 
+        } catch(err) {
+            console.error(err)
+        }
     }
 }
