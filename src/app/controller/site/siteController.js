b6b4753126617be367d7135af269d53b0b504553
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-25 11:30:09 -0300

    add recipe image in recipe site

--- src/app/controller/site/siteController.js
+++ src/app/controller/site/siteController.js
@@ -1,12 +1,6 @@
 const Site = require('../../models/site/Site')
 
 module.exports = {
-    async home(req, res) {
-        let results = await Site.all() 
-        const recipes = results.rows
-        
-        return res.render("site/home", { recipes })
-    },
     about(req, res) {
         return res.render("site/about")
     },
@@ -14,7 +8,23 @@ module.exports = {
         let results = await Site.all()
         const recipes = results.rows 
         
-        return res.render("site/recipes", { recipes })
+        if (!recipes) return res.send("Recipes not found")
+
+        async function getImage(recipeId) {
+            let results = await Site.files(recipeId)
+            const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+
+            return files[0]
+        }
+
+        const recipesPromise = recipes.map(async recipe => {
+            recipe.img = await getImage(recipe.id)
+            return recipe
+        }).filter((recipe, index) => index > 2 ? false : true)
+
+        const lastAdded = await Promise.all(recipesPromise)
+
+        return res.render("site/recipes", { recipes: lastAdded })
         
     },
     async details(req, res) {
