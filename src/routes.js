59464c71cb21eb9ce6024b3031b82b66f1e46a78
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-29 20:22:24 -0300

    adjust the search page

--- src/routes.js
+++ src/routes.js
@@ -1,42 +1,39 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
-const siteController = require('./app/controller/site/siteController')
-const recipesController = require('./app/controller/admin/recipesController')
-const chefsController = require('./app/controller/admin/chefsController')
+const SiteController = require('./app/controller/site/SiteController')
+const RecipesController = require('./app/controller/admin/RecipesController')
+const ChefsController = require('./app/controller/admin/ChefsController')
 const HomeController = require("./app/controller/site/HomeController")
 const SearchController = require("./app/controller/site/SearchController")
 
 /*=== Routes Site ===*/
 routes.get('/', HomeController.index)
-
-// Search
 routes.get('/search', SearchController.index)
-
-routes.get('/about', siteController.about)
-routes.get('/recipes', siteController.recipes)
-routes.get('/recipes/:id', siteController.details)
-routes.get('/chefs', siteController.chefs)
+routes.get('/about', SiteController.about)
+routes.get('/recipes', SiteController.recipes)
+routes.get('/recipes/:id', SiteController.details)
+routes.get('/chefs', SiteController.chefs)
 
 /*=== Routes Admin Recipes ===*/
-routes.get('/admin/recipes', recipesController.index)
-routes.get('/admin/recipes/create', recipesController.create)
-routes.get('/admin/recipes/:id', recipesController.show)
-routes.get('/admin/recipes/:id/edit', recipesController.edit)
+routes.get('/admin/recipes', RecipesController.index)
+routes.get('/admin/recipes/create', RecipesController.create)
+routes.get('/admin/recipes/:id', RecipesController.show)
+routes.get('/admin/recipes/:id/edit', RecipesController.edit)
 
-routes.post('/admin/recipes', multer.array("photos", 5), recipesController.post)
-routes.put('/admin/recipes', multer.array("photos", 5), recipesController.put)
-routes.delete('/admin/recipes', recipesController.delete)
+routes.post('/admin/recipes', multer.array("photos", 5), RecipesController.post)
+routes.put('/admin/recipes', multer.array("photos", 5), RecipesController.put)
+routes.delete('/admin/recipes', RecipesController.delete)
 
 /*=== Routes Admin chefs ===*/
-routes.get('/admin/chefs', chefsController.index)
-routes.get('/admin/chefs/create', chefsController.create)
-routes.get('/admin/chefs/:id', chefsController.show)
-routes.get('/admin/chefs/:id/edit', chefsController.edit)
+routes.get('/admin/chefs', ChefsController.index)
+routes.get('/admin/chefs/create', ChefsController.create)
+routes.get('/admin/chefs/:id', ChefsController.show)
+routes.get('/admin/chefs/:id/edit', ChefsController.edit)
 
-routes.post('/admin/chefs', multer.array("photos", 1), chefsController.post)
-routes.put('/admin/chefs', multer.array("photos", 1), chefsController.put)
-routes.delete('/admin/chefs', chefsController.delete)
+routes.post('/admin/chefs', multer.array("photos", 1), ChefsController.post)
+routes.put('/admin/chefs', multer.array("photos", 1), ChefsController.put)
+routes.delete('/admin/chefs', ChefsController.delete)
 
 /*=== Not Found ===*/
 routes.use(function(req, res) {
