1f229ad6562bb28c7efa3b10879dd2711cfc8834
Jacó Apolinário <jacoapollinario@gmail.com>
2021-03-12 11:57:28 -0300

    add password user

--- src/app/controller/admin/UserController.js
+++ src/app/controller/admin/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../../models/admin/user')
 
+
 module.exports = {
     create(req, res) {
         return res.render("admin/user/create")
@@ -8,6 +9,8 @@ module.exports = {
 
         const userId = await User.create(req.body)
 
-        return res.send("Ok, Cadastrado")
+        return res.render("admin/user/create", {
+            success: "Usuário Cadastrado!"
+        })
     }
 }
--- src/app/models/admin/user.js
+++ src/app/models/admin/user.js
@@ -1,4 +1,7 @@
 const db = require('../../../config/db')
+const crypto = require('crypto')
+const { hash } = require('bcryptjs')
+const mailer = require('../../../lib/mailer')
 
 module.exports = {
     async findOne(filters) {
@@ -24,19 +27,41 @@ module.exports = {
                 INSERT INTO users (
                     name,
                     email,
+                    password,
                     is_admin
-                ) VALUES ($1, $2, $3)
+                ) VALUES ($1, $2, $3, $4)
                 RETURNING id
             `
+            // Generate password
+            const password = crypto.randomBytes(8).toString("hex")
+
+           // Send email password
+            await mailer.sendMail({
+                to: data.email,
+                from: 'no-reply@foodfy.com.br',
+                subject: 'Foodfy - Seja bem-vindo',
+                html: `<h2>Cadastro realizado com sucesso no Foodfy</h2>
+                <p>
+                Sua senha: ${password}<br>
+                Guarde-a em local seguro.
+                </p>`
+            })
+
+
+            // hash of password
+            const passwordHash = await hash(password, 8)
 
             const values = [
                 data.name,
                 data.email,
+                passwordHash,
                 data.is_admin
             ]
 
+
             const results = await db.query(query, values)
             return results.rows[0].id
+
 
         } catch(err) {
             console.error(err)
