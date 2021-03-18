b7cf590fd5e18af898fa2d994e0545e847575f3b
Jacó Apolinário <jacoapollinario@gmail.com>
2021-03-18 11:24:19 -0300

    add user login

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const routes = express.Router()
+const SessionController = require('../app/controller/admin/SessionController')
 const UserController = require('../app/controller/admin/UserController')
 const ProfileController = require('../app/controller/admin/ProfileController')
 
 const UserValidator = require('../app/validators/user')
 
+// Login/logout
+routes.get('/users/login', SessionController.loginForm)
+// routes.post('/login', SessionController.login)
+// routes.post('logout', SessionController.logout)
 
 // Profile
-routes.get('/admin/profile', ProfileController.index) 
-routes.put('/admin/profile', ProfileController.put)
-
+routes.get('/users/profile', ProfileController.index) 
+routes.put('/users/profile', ProfileController.put)
 
 // Users register
 routes.get('/users/create', UserController.create)
