d5ed133045697b4b3708d5b5138e29260d15c8fe
Jacó Apolinário <jacoapollinario@gmail.com>
2021-03-18 11:50:19 -0300

    add email validate

---
+++ public/scripts/validate.js
@@ -0,0 +1,36 @@
+const Validate = {
+    apply(input, func) {
+        Validate.clearErros(input)
+
+        let results = Validate[func](input.value)
+        input.value = results.value
+
+        if (results.error)
+            Validate.displayError(input, results.error)
+    },
+    displayError(input, error) {
+        const div = document.createElement('div')
+        div.classList.add('error')
+        div.innerHTML = error
+        input.parentNode.appendChild(div)
+        input.focus()
+    },
+    clearErros(input){
+        const errorDiv = input.parentNode.querySelector(".error")
+        if (errorDiv)
+            errorDiv.remove()
+    },
+    isEmail(value) {
+        let error = null
+
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        if (!value.match(mailFormat))
+            error = "Email inválido"
+
+        return {
+            error,
+            value
+        }
+    }
+}
